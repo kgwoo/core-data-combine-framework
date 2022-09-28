cbb4187825c31f2cb179c9ae4de3ad9f2a3c39b4
Gunwoo Kim <gunoo0126@gmail.com>
2022-09-28 11:29:42 +0900

    Initial Commit

---
+++ core-data-combine-framework/core_data_combine_frameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  core_data_combine_frameworkApp.swift
+//  core-data-combine-framework
+//
+//  Created by GunWoo on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct core_data_combine_frameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
